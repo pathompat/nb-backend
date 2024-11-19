4ad0ea255cecead35fcc14509382fb1ca9000e1a
pathompat <lw93430@linecorp.com>
2024-11-19 23:48:42 +0700

    add cors

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/gin-contrib/cors v1.7.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -15,7 +16,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 )
 
--- main.go
+++ main.go
@@ -6,27 +6,30 @@ import (
 	"notebook-backend/router"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// setup env
 	err := godotenv.Load()
 	if err != nil {
 		log.Print("Error loading .env file")
 	}
 
+	// database setup
 	dbUsername := os.Getenv("DB_USERNAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
-
 	db := config.InitDB(dbUsername, dbPassword, dbName, dbHost, dbPort)
 
+	// setup routes
 	r := gin.Default()
 
-	// Setup routes
+	r.Use(cors.Default())
 	router.SetupRoutes(r, db)
 
 	r.Run()
