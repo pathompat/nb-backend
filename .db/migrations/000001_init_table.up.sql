149880fe1aed3b4ba34d44a76db44b563cdf11de
pathompat <lw93430@linecorp.com>
2024-12-01 23:54:15 +0700

    fix format

--- .db/migrations/000001_init_table.up.sql
+++ .db/migrations/000001_init_table.up.sql
@@ -1,7 +1,7 @@
 -- create table user
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    uuid uuid NOT NULL DEFAULT uuid_generate_v4 () ,
+    uuid uuid NOT NULL DEFAULT uuid_generate_v4(),
     username VARCHAR(50) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     store_name VARCHAR(100),
