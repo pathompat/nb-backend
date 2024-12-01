c086b4d401e2482bbbe1315397157e094a2873fe
pathompat <lw93430@linecorp.com>
2024-12-01 20:12:47 +0700

    add hot reload

--- config/database.go
+++ config/database.go
@@ -11,7 +11,6 @@ import (
 func InitDB(username string, password string, database string, host string, port string) *gorm.DB {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		host, username, password, database, port)
-	// dsn := "host=localhost user=youruser password=yourpassword dbname=yourdb port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
