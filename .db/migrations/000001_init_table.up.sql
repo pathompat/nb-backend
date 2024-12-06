90bbb9e50c5ba815c422c74508f174c02b20cb48
pathompat <lw93430@linecorp.com>
2024-12-06 23:30:19 +0700

    update migrate script

--- .db/migrations/000001_init_table.up.sql
+++ .db/migrations/000001_init_table.up.sql
@@ -4,9 +4,9 @@ CREATE TABLE users (
     uuid uuid NOT NULL DEFAULT uuid_generate_v4(),
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    store_name VARCHAR(100),
+    store_name VARCHAR(100) NOT NULL ,
     tier_id SMALLINT,
-    role VARCHAR(20) DEFAULT 'CUSTOMER',
+    role VARCHAR(20) NOT NULL DEFAULT 'CUSTOMER',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP
@@ -17,10 +17,11 @@ CREATE TABLE schools (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     name VARCHAR(100) NOT NULL,
-    address VARCHAR(255),
-    telephone VARCHAR(11),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    address VARCHAR(255) NOT NULL,
+    telephone VARCHAR(11) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
@@ -29,16 +30,17 @@ CREATE TABLE quotations (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     school_id INT NOT NULL,
-    store_name VARCHAR(100),
-    school_name VARCHAR(100),
-    school_address VARCHAR(255),
-    school_telephone VARCHAR(11),
-    is_instant BOOLEAN,
+    store_name VARCHAR(100) NOT NULL,
+    school_name VARCHAR(100) NOT NULL,
+    school_address VARCHAR(255) NOT NULL,
+    school_telephone VARCHAR(11) NOT NULL,
     appointment_at TIMESTAMP,
-    duedate_at TIMESTAMP,
-    status VARCHAR(20),
+    duedate_at TIMESTAMP NOT NULL,
+    remark TEXT,
+    status VARCHAR(20) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
     CONSTRAINT fk_user_quotation FOREIGN KEY (user_id) REFERENCES users(id),
     CONSTRAINT fk_school FOREIGN KEY (school_id) REFERENCES schools(id)
 );
@@ -47,17 +49,15 @@ CREATE TABLE quotations (
 CREATE TABLE quotation_items (
     id SERIAL PRIMARY KEY,
     quotation_id INT NOT NULL,
-    product VARCHAR(100),
+    product_title VARCHAR(100) NOT NULL,
     plate VARCHAR(20),
-    gram INT,
-    color INT,
-    page INT,
-    line VARCHAR(50),
-    has_ref BOOLEAN,
-    quantity INT,
-    price INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    gram INT NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    page INT NOT NULL,
+    pattern VARCHAR(50) NOT NULL,
+    has_reference BOOLEAN NOT NULL,
+    quantity INT NOT NULL,
+    price DOUBLE PRECISION NOT NULL,
     CONSTRAINT fk_quotation FOREIGN KEY (quotation_id) REFERENCES quotations(id)
 );
 
@@ -67,8 +67,10 @@ CREATE TABLE productions (
     user_id INT NOT NULL,
     school_id INT NOT NULL,
     quotation_id INT NOT NULL,
+    remark TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
     CONSTRAINT fk_user_production FOREIGN KEY (user_id) REFERENCES users(id),
     CONSTRAINT fk_school_production FOREIGN KEY (school_id) REFERENCES schools(id),
     CONSTRAINT fk_quotation_production FOREIGN KEY (quotation_id) REFERENCES quotations(id)
@@ -77,29 +79,33 @@ CREATE TABLE productions (
 -- create table production_items
 CREATE TABLE production_items (
     id SERIAL PRIMARY KEY,
-    prod_id INT NOT NULL,
-    product VARCHAR(100),
+    production_id INT NOT NULL,
+    product_title VARCHAR(100) NOT NULL,
     plate VARCHAR(20),
-    gram INT,
-    color INT,
-    page INT,
-    line VARCHAR(50),
-    has_ref BOOLEAN,
-    status VARCHAR(20),
+    gram INT NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    page INT NOT NULL,
+    pattern VARCHAR(50) NOT NULL,
+    has_reference BOOLEAN NOT NULL,
+    status VARCHAR(20) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_production FOREIGN KEY (prod_id) REFERENCES productions(id)
+    deleted_at TIMESTAMP,
+    CONSTRAINT fk_production FOREIGN KEY (production_id) REFERENCES productions(id)
 );
 
 -- create table price references
 CREATE TABLE price_references (
     id SERIAL PRIMARY KEY,
     tier_id SMALLINT NOT NULL,
-    product VARCHAR(100),
-    gram INT,
-    color INT,
-    page INT,
-    line VARCHAR(50),
+    product_title VARCHAR(100) NOT NULL,
+    plate VARCHAR(20),
+    gram INT NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    page INT NOT NULL,
+    pattern VARCHAR(50) NOT NULL,
+    has_reference BOOLEAN NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
 );
