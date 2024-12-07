4f76dc59e64c3b58a7aa2e2db538a75080763438
pathompat <lw93430@linecorp.com>
2024-12-07 19:38:27 +0700

    update stuct

--- handler/dto/quotation.go
+++ handler/dto/quotation.go
@@ -10,6 +10,15 @@ type QuotationFilter struct {
 	IncludeProduction bool `form:"includeProduction"`
 }
 
+type CreateQuotation struct {
+	UserID        uuid.UUID       `json:"userId" binding:"required,uuid4" example:"78705ee5-25cd-45b5-8cb1-63f1cb94e5c8"` // Owner uuid                  // Store name
+	SchoolID      uint            `json:"schoolId" binding:"required,gt=0" example:"2"`                                   // School id
+	AppointmentAt *time.Time      `json:"appointmentAt" binding:"-" example:"2024-12-00:00:00.0000+07:00"`                // Appointment date (null is now)
+	DueDateAt     time.Time       `json:"dueDateAt" binding:"required" example:"2024-12-06"`                              // Last due date
+	Items         []QuotationItem `json:"items" binding:"required,dive"`                                                  // Quotation product list
+	Remark        string          `json:"remark" example:"remark test"`                                                   // Any remark
+}
+
 type QuotationResponse struct {
 	ID              uint                `json:"id" example:"1"`                                        // Document id
 	UserID          uuid.UUID           `json:"userId" example:"78705ee5-25cd-45b5-8cb1-63f1cb94e5c8"` // Owner uuid
@@ -20,20 +29,21 @@ type QuotationResponse struct {
 	AppointmentAt   *time.Time          `json:"appointmentAt" example:"2024-12-02"`                    // Appointment date (null is now)
 	DueDateAt       time.Time           `json:"dueDateAt" example:"2024-12-02"`                        // Last due date
 	Status          string              `json:"status" example:"REVIEWING"`                            // Document status (REVIEWING, APPROVED, CANCELED)
+	Remark          string              `json:"remark" example:"test remark"`                          // Document remark
 	Production      *ProductionResponse `json:"production,omitempty"`                                  // Production related
 	Items           []QuotationItem     `json:"items"`                                                 // Quotation product list
-	CreatedAt       time.Time           `json:"createdAt" example:"2024-12-02T00:26:21.087061Z"`       // Created date
-	UpdatedAt       time.Time           `json:"updatedAt" example:"2024-12-02T00:26:21.087061Z"`       // Latest update date
+	CreatedAt       time.Time           `json:"createdAt" example:"2024-12-07T19:04:39.70268+07:00"`   // Created date
+	UpdatedAt       time.Time           `json:"updatedAt" example:"2024-12-07T19:04:39.70268+07:00"`   // Latest update date
 }
 
 type QuotationItem struct {
-	ProductTitle string  `json:"productTitle" example:"Cut 8"` // Product name
-	Plate        string  `json:"plate" example:"LARGE"`        // Plate size (LARGE, SMALL)
-	Gram         int     `json:"gram" example:"40"`            // Notebook grams (40-150)
-	Color        string  `json:"color" example:"1"`            // Color (1,4)
-	Page         int     `json:"page" example:"40"`            // Page count (30-80)
-	Pattern      string  `json:"pattern" example:"TABLE"`      // Page pattern
-	HasReference bool    `json:"hasReference" example:"false"` // Has reference
-	Quantity     int     `json:"quantity" example:"1000"`      // Product quantity
-	Price        float32 `json:"price" example:"5.5"`          // Product price
+	ProductTitle string  `json:"productTitle" binding:"required" example:"Cut 8"`      // Product name
+	Plate        string  `json:"plate" binding:"-" example:"LARGE"`                    // Plate size (LARGE, SMALL)
+	Gram         int     `json:"gram" binding,gte=5:"required" example:"40"`           // Notebook grams (40-150)
+	Color        string  `json:"color" binding:"required" example:"1"`                 // Color (1,4)
+	Page         int     `json:"page" binding,gte=10:"required" example:"40"`          // Page count (30-80)
+	Pattern      string  `json:"pattern" binding:"required,uppercase" example:"TABLE"` // Page pattern
+	HasReference *bool   `json:"hasReference" binding:"required" example:"false"`      // Has reference
+	Quantity     int     `json:"quantity" binding:"required,gte=1" example:"1000"`     // Product quantity
+	Price        float32 `json:"price" binding:"required,gt=0" example:"5.5"`          // Product price
 }
