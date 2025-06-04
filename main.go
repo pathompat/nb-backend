0130d83b9f2614afe6ce74bafb01e1ed33aaec99
pathompat <lw93430@linecorp.com>
2025-06-04 23:12:35 +0700

    fix cors

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://api-test.tickbook.net"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://test.tickbook.net"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
