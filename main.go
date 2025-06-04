b8d9d2d11d2d810caa155cabc9feaed86a297d50
pathompat <lw93430@linecorp.com>
2025-06-04 23:11:32 +0700

    allow cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"notebook-backend/config"
 	"notebook-backend/helper"
 	"os"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -55,7 +56,14 @@ func main() {
 	// setup routes
 	r := gin.Default()
 
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "https://api-test.tickbook.net"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	config.SetupRoutes(r, db)
 
 	r.Run()
